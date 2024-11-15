6e510a1d5ca93798cba126f96396f229cf76ee81
Artem <artemklymenko892@gmail.com>
2024-11-15 16:33:13 +0200

    Create CoinListState

---
+++ app/src/main/java/com/artemklymenko/mycryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,11 @@
+package com.artemklymenko.mycryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Immutable
+import com.artemklymenko.mycryptotracker.crypto.presentation.models.CoinUi
+
+@Immutable
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: CoinUi? = null
+)
