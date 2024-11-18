5479aa4fa5e53859fe01d178314e8b1e28e60775
Artem <artemklymenko892@gmail.com>
2024-11-18 10:57:31 +0200

    Create constructUrl

---
+++ app/src/main/java/com/artemklymenko/mycryptotracker/crypto/core/data/networking/constructUrl.kt
@@ -0,0 +1,11 @@
+package com.artemklymenko.mycryptotracker.crypto.core.data.networking
+
+import com.artemklymenko.mycryptotracker.BuildConfig
+
+fun constructUrl(url: String): String{
+    return when {
+        url.contains(BuildConfig.BASE_URL) -> url
+        url.startsWith("/") -> BuildConfig.BASE_URL + url.drop(1)
+        else -> BuildConfig.BASE_URL + url
+    }
+}
