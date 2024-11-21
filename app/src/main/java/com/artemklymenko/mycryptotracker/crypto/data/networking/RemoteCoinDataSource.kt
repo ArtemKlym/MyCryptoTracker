26f6d654d2a8268a187e77a5c5ce400bdfaeefa8
Artem <artemklymenko892@gmail.com>
2024-11-21 12:38:49 +0200

    Implement getCoinHistory in RemoteCoinDataSource

--- app/src/main/java/com/artemklymenko/mycryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/artemklymenko/mycryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.artemklymenko.mycryptotracker.core.domain.util.NetworkError
 import com.artemklymenko.mycryptotracker.core.domain.util.Result
 import com.artemklymenko.mycryptotracker.core.domain.util.map
 import com.artemklymenko.mycryptotracker.crypto.data.mappers.toCoin
+import com.artemklymenko.mycryptotracker.crypto.data.mappers.toCoinPrice
+import com.artemklymenko.mycryptotracker.crypto.data.networking.dto.CoinHistoryDto
 import com.artemklymenko.mycryptotracker.crypto.data.networking.dto.CoinsResponseDto
 import com.artemklymenko.mycryptotracker.crypto.domain.Coin
 import com.artemklymenko.mycryptotracker.crypto.domain.CoinDataSource
+import com.artemklymenko.mycryptotracker.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
@@ -22,6 +28,32 @@ class RemoteCoinDataSource(
             )
         }.map { response ->
             response.data.map { it.toCoin() }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMills = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        val endMills = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        return safeCall<CoinHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6")
+                parameter("start", startMills)
+                parameter("end", endMills)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
         }
     }
 }
