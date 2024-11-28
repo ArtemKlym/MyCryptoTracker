2fe035a5b69da4bed100d9fb77e823cad14e4076
Artem <artemklymenko892@gmail.com>
2024-11-28 12:26:30 +0200

    Add filteredCoins in CoinListState

--- app/src/main/java/com/artemklymenko/mycryptotracker/crypto/presentation/coin_list/CoinListState.kt
+++ app/src/main/java/com/artemklymenko/mycryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -7,6 +7,7 @@ import com.artemklymenko.mycryptotracker.crypto.presentation.models.CoinUi
 data class CoinListState(
     val isLoading: Boolean = false,
     val coins: List<CoinUi> = emptyList(),
+    val filteredCoins: List<CoinUi> = emptyList(),
     val selectedInterval: String = "m15",
     val selectedCoin: CoinUi? = null
 )
