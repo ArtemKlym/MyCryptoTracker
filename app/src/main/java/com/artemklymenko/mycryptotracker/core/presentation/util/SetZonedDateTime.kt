1f4c129c29f9975a8462943881e82e50015e7dc7
Artem <artemklymenko892@gmail.com>
2024-12-02 11:08:59 +0200

    Change time ranges in SetZonedDateTime.kt

--- app/src/main/java/com/artemklymenko/mycryptotracker/core/presentation/util/SetZonedDateTime.kt
+++ app/src/main/java/com/artemklymenko/mycryptotracker/core/presentation/util/SetZonedDateTime.kt
@@ -4,13 +4,13 @@ import java.time.ZonedDateTime
 
 fun setStartZonedDateTime(interval: String): ZonedDateTime {
     return when {
-        interval.contains("m1") -> ZonedDateTime.now().minusMinutes(5)
-        interval.contains("m5") -> ZonedDateTime.now().minusMinutes(30)
-        interval.contains("m30") -> ZonedDateTime.now().minusHours(6)
-        interval.contains("h1") -> ZonedDateTime.now().minusHours(12)
-        interval.contains("h2") -> ZonedDateTime.now().minusHours(24)
-        interval.contains("h6") -> ZonedDateTime.now().minusDays(3)
-        interval.contains("d1") -> ZonedDateTime.now().minusDays(5)
+        interval.contains("m1") -> ZonedDateTime.now().minusMinutes(20)
+        interval.contains("m5") -> ZonedDateTime.now().minusMinutes(80)
+        interval.contains("m30") -> ZonedDateTime.now().minusHours(10)
+        interval.contains("h1") -> ZonedDateTime.now().minusHours(18)
+        interval.contains("h2") -> ZonedDateTime.now().minusHours(32)
+        interval.contains("h6") -> ZonedDateTime.now().minusDays(6)
+        interval.contains("d1") -> ZonedDateTime.now().minusWeeks(2)
         else -> ZonedDateTime.now().minusDays(5)
     }
 }
